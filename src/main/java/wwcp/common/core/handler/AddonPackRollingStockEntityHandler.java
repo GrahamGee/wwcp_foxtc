81d36b50818e25f753d0646f3b56f06157b39f9f
TBEA <74006945+RealTBEA@users.noreply.github.com>
2025-12-04 23:26:34 -0500

    Converted All Model Classes

---
+++ src/main/java/wwcp/common/core/handler/AddonPackRollingStockEntityHandler.java
@@ -0,0 +1,28 @@
+package wwcp.common.core.handler;
+
+import wwcp.common.wwcp;
+
+public class AddonPackRollingStockEntityHandler
+{
+    private Object Instance()
+    {
+        return wwcp.instance;
+    }
+
+    public AddonPackRollingStockEntityHandler()
+    {
+        /* Sample
+
+        Traincraft.traincraftRegistry
+                .RegisterRollingStockEntity
+                        (ItemIDs.minecartPassengerBlue.item,
+                                new TrainRecord("Passenger Blue", EntityPassengerBlue.class, ItemIDs.minecartPassengerBlue.item, EnumTrainType.Passenger, 1,
+                                        new String[]{"Blue", "Red", "Green", "White", "Black", "Cyan", "Orange", "Grey", "LightGrey", "LightBlue"},
+                                        18),
+                                Instance() // don't touch this line
+                        );
+         */
+
+        // Put Calls to RegisterRollingStockEntity below this.
+    }
+}
